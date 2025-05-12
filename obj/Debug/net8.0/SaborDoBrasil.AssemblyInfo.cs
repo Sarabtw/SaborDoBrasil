5ef9fc62f0ecfe7b4d48b50617721a440ab397c4
Larissa de Oliveira <larissaoliveira3zb@gmail.com>
2025-05-12 16:45:24 -0300

    fix

--- obj/Debug/net8.0/SaborDoBrasil.AssemblyInfo.cs
+++ obj/Debug/net8.0/SaborDoBrasil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2970fe23cc1c41d5d03949c7ee1e2f273d6dc945")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f236785712e260bdb06001d718b29e7b76fffe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
